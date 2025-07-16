79e884b1707cecc1e8dc412478c63b45314dcb62
immanuellking <emmaebubeegbosi@gmail.com>
2025-07-16 20:59:26 +0100

    implmented loading skeleton

---
+++ src/components/skeleton/index.tsx
@@ -0,0 +1,12 @@
+const NewsCardSkeleton = () => {
+  const skeleton = Array.from({ length: 12 }, () => null);
+  return (
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-4 sm:gap-y-16 sm:gap-x-4 sm:mt-8 lg:mt-16">
+      {skeleton.map((_, index) => (
+        <div key={index} className="w-full aspect-square bg-[#2A283E] animate-pulse"></div>
+      ))}
+    </div>
+  );
+};
+
+export default NewsCardSkeleton;
--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,11 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import NewsCard from "../../components/news-card";
+import NewsCardSkeleton from "../../components/skeleton";
 
 const News = () => {
   const API_URL = `https://finnhub.io/api/v1/news?category=general&token=${
     import.meta.env.VITE_NEWS_API_KEY
   }`;
-  const { data } = useQuery({
+  const { data, isFetching } = useQuery({
     queryKey: ["news"],
     queryFn: () => {
       return fetch(`${API_URL}`).then((response) => response.json());
@@ -15,19 +16,25 @@ const News = () => {
   console.log(data);
   return (
     <section className="sm:w-[90%] mx-auto">
-      <h1 className="font-rubik font-bold sm:font-medium text-2xl sm:text-5xl text-white px-2 sm:px-0 py-4">NEWS</h1>
+      <h1 className="font-rubik font-bold sm:font-medium text-2xl sm:text-5xl text-white px-2 sm:px-0 py-4">
+        NEWS
+      </h1>
+
+      {isFetching && <NewsCardSkeleton />}
 
       <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-4 sm:gap-y-16 sm:gap-x-4 sm:mt-8 lg:mt-16">
-        {data?.map((item: any, index: number) => (
-          <NewsCard
-            key={index}
-            image={item.image}
-            source={item.source}
-            url={item.url}
-            datetime={item.datetime}
-            headline={item.headline}
-          />
-        ))}
+        {data &&
+          data.length > 0 &&
+          data?.map((item: any, index: number) => (
+            <NewsCard
+              key={index}
+              image={item.image}
+              source={item.source}
+              url={item.url}
+              datetime={item.datetime}
+              headline={item.headline}
+            />
+          ))}
       </div>
     </section>
   );
