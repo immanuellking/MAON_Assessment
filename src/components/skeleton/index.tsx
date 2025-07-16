6ceff4ee89b894153c438c76c7bdcb825427f4d9
immanuellking <emmaebubeegbosi@gmail.com>
2025-07-16 22:32:51 +0100

    made skeleton responsive for mobile

--- src/components/skeleton/index.tsx
+++ src/components/skeleton/index.tsx
@@ -3,7 +3,7 @@ const NewsCardSkeleton = () => {
   return (
     <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-4 sm:gap-y-16 sm:gap-x-4 sm:mt-8 lg:mt-16">
       {skeleton.map((_, index) => (
-        <div key={index} className="w-full aspect-square bg-[#2A283E] animate-pulse"></div>
+        <div key={index} className="w-full h-[110px] sm:h-auto sm:aspect-square bg-[#2A283E] animate-pulse"></div>
       ))}
     </div>
   );
